4456ae2b9a513df560680485f8b080574e845331
Dejan Toteff <deyan8284@gmail.com>
2020-03-13 22:41:35 +0200

    chore: import from joke-maker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router'
+import {FooComponent} from './foo/foo.component'
+import {WordsComponent} from './words/words.component'
+import {RoughDemoComponent} from './rough-demo/rough-demo.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RoughDemoComponent},
+  {path: 'words', component: WordsComponent},
+  {path: 'foo', component: FooComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/helpers/foo.ts
@@ -0,0 +1,4 @@
+export function foo(a: number) {
+  // if(!a) return 4
+  return 2 + a
+}
