6e44180b3ef37d458790964764a148ed54d3cd14
Dejan Toteff <deyan8284@gmail.com>
2020-03-15 20:01:21 +0200

    feat@stop repl | handle repl change tested

--- src/app/_modules/handle-repl-change.spec.ts
+++ src/app/_modules/handle-repl-change.spec.ts
@@ -1,9 +1,66 @@
-import {addConst, handleReplChange} from './handle-repl-change'
+import * as R from "rambdax";
 
-test('adds const when it is missing', () => {
-  const withoutConst = 'R.add(1,2)'
-  const withConst = `const result = ${withoutConst}`
-  const expected = `"const result = R.add(1,2)"`
-  expect(addConst(withoutConst)).toMatchInlineSnapshot(expected)
-  expect(addConst(withConst)).toMatchInlineSnapshot(expected)
-})
+import { addConst, handleReplChange } from "./handle-repl-change";
+
+test("adds const when it is missing", () => {
+  const withoutConst = "R.add(1,2)";
+  const withConst = `const result = ${withoutConst}`;
+  const expected = '"const result = R.add(1,2)"';
+  expect(addConst(withoutConst)).toMatchInlineSnapshot(expected);
+  expect(addConst(withConst)).toMatchInlineSnapshot(expected);
+});
+
+test("sync", async () => {
+  const code = `
+  const fn = R.compose(
+    R.join('|'),
+    R.map(R.add(2)),
+    R.filter(x => x > 2),
+    R.map(x => x *2 )
+  )
+  const result = fn([1,2,3])  
+  `;
+  const result = await handleReplChange(code, R);
+  expect(result).toMatchInlineSnapshot(`"\\"6|8\\""`);
+});
+
+test("sync error", async () => {
+  const code = `
+  const result = R.compose(
+    R.join('|'),
+    R.map(),
+  )(1)
+  `;
+  const result = await handleReplChange(code, R);
+  expect(result).toMatchInlineSnapshot(`"fn is not a function"`);
+});
+
+test("async", async () => {
+  const code = `
+  const result = R.composeAsync(
+    x => ({x}),
+    R.map(R.add(2)),
+    R.filter(x => x > 2),
+    R.map(x => x *2 )
+  )([1,2,3])
+  `;
+  const result = await handleReplChange(code, R);
+  expect(result).toMatchInlineSnapshot(`
+"{
+  \\"x\\": [
+    6,
+    8
+  ]
+}"
+`);
+});
+
+test("async error", async () => {
+  const code = `
+  const result = R.composeAsync(
+    R.map()
+  )([1,2,3])
+  `;
+  const result = await handleReplChange(code, R);
+  expect(result).toMatchInlineSnapshot(`"fn is not a function"`);
+});
--- src/app/_modules/handle-repl-change.ts
+++ src/app/_modules/handle-repl-change.ts
@@ -1,4 +1,4 @@
-import {map, type, anyPass, includes, unless} from 'rambdax'
+import {type, anyPass, includes, unless, RambdaTypes} from 'rambdax'
 
 const hasResultConst = anyPass([
   includes('const result ='),
@@ -7,9 +7,70 @@ const hasResultConst = anyPass([
   includes('let result ='),
 ])
 
-export const addConst = unless(hasResultConst, x => `const result = ${x}`)
+interface ResultHolder {
+  async: boolean
+  payload: any
+  type: RambdaTypes
+}
 
-export function handleReplChange(codeRaw) {
-  const code = addConst(codeRaw)
-  return code
+export const addConst = unless(hasResultConst, x => `const result = ${x}`)
+export const addSemiColon = unless(
+  x => x.endsWith(';'),
+  x => `${x};`,
+)
+export const stringifyResult = x => {
+  if (type(x) === 'Object') {
+    return JSON.stringify(x, null, 2)
+  }
+
+  return JSON.stringify(x)
+}
+
+async function evaluateCode(codeToEvaluate, maybeRambdaxMock): Promise<string | ResultHolder> {
+  if (maybeRambdaxMock) {
+    var R = maybeRambdaxMock
+  }
+  var resultHolder: ResultHolder
+  try {
+    eval(codeToEvaluate)
+    return resultHolder
+  } catch (e) {
+    return e.message === undefined ? 'ERROR' : e.message
+  }
+}
+
+export function handleReplChange(input, maybeRambdaxMock) {
+  return new Promise(resolve => {
+    const code = addSemiColon(addConst(input.trim()))
+    const codeToEvaluate = `
+    ${code}
+    const resultType = R.type(result)
+
+    resultHolder = {
+      async: ['Promise', 'Async'].includes(resultType),
+      type: resultType,
+      payload: result
+    }
+    `
+    evaluateCode(codeToEvaluate, maybeRambdaxMock).then(evaluated => {
+      if (typeof evaluated === 'string') {
+        return resolve(evaluated)
+      }
+      const resultHolder = evaluated
+
+      if (!['Async', 'Promise'].includes(resultHolder.type)) {
+        return resolve(stringifyResult(resultHolder.payload))
+      }
+
+      const promised = resultHolder.type === 'Async' ? resultHolder.payload() : resultHolder.payload
+
+      promised
+        .then(promisedResult => {
+          resolve(stringifyResult(promisedResult))
+        })
+        .catch(e => {
+          resolve(e.message === undefined ? 'ERROR' : e.message)
+        })
+    })
+  })
 }
