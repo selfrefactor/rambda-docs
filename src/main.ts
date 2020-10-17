be068c1b11a6aec7666ba0117538d25f3d99e799
Dejan Toteff <deyan8284@gmail.com>
2020-10-17 17:24:32 +0300

    chore@dnd

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import 'codemirror/mode/javascript/javascript'
-import {enableProdMode} from '@angular/core'
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
 import * as R from 'rambdax'
 window.R = R
 
 import {AppModule} from './app/app.module'
-import {environment} from './environments/environment'
 
-if (environment.production) {
-  enableProdMode()
-}
+// if (environment.production) {
+//   enableProdMode()
+// }
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch(err => console.error(err))
+ 
