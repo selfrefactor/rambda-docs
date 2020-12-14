8d2de413cf6bdbfe4354118e6b153cc938b130d1
Dejan Totev <dejan.totev@psinteractive.io>
2020-12-14 22:13:30 +0200

    feat@dep use playwright.screens

--- e2e/snap.spec.ts
+++ e2e/snap.spec.ts
@@ -1,24 +1,33 @@
 import {ms} from 'string-fn'
-import {mapAsync} from 'rambdax'
+import {playwrightScreens} from 'playwright-screens'
+import { WrapOutput } from 'playwright-wrap'
 const urlBase = 'http://localhost:4200'
 const testUrl = `${urlBase}/all`
-import {TestData, snap} from './snap'
 
 jest.setTimeout(ms('2 minutes'))
 
-const TEST_DATA: TestData[] = [
-  {label: 'tiny', screen: {x: 1024, y: 768}},
-  {label: 'smaller', screen: {x: 1280, y: 720}},
-  {label: 'small', screen: {x: 1366, y: 768}},
-  {label: 'medium', screen: {x: 1600, y: 900}},
-  {label: 'big', screen: {x: 1920, y: 1080}},
-  {label: 'huge', screen: {x: 2256, y: 1504}},
-]
+function waitFor(_: WrapOutput) {
+  return async() => {
+    const el = await _.page.$('#test-id')
+    if (!el) throw new Error('no element with #test-id')
+    const replReadyIndicator = await el.getAttribute('data-repl-ready')
+
+    return replReadyIndicator === 'true'
+  }
+}
+
+async function waitForReady(_: WrapOutput){
+  await _.waitForPredicate(waitFor(_), 10000)
+  return true
+}
+
 
 test('happy', async() => {
   console.time('snap')
-  await mapAsync(async x => {
-    await snap(testUrl, x)
-  }, TEST_DATA)
+
+  await playwrightScreens({
+    url: testUrl,
+    waitForReady
+  })
   console.timeEnd('snap')
 })
